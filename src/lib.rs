adb1b7e6d225f6233ba455d8f7c28de4cd0084a7
Lance Hart <shipsimfan@hotmail.com>
2022-01-15 18:16:32 -0500

    Changed log macros to use references

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,7 @@ pub use record::*;
 
 #[doc(hidden)]
 pub fn _log(
-    logger: Logger,
+    logger: &Logger,
     level: LogLevel,
     file: &str,
     line_number: u32,
@@ -29,40 +29,40 @@ pub fn _log(
 
 #[macro_export]
 macro_rules! emergency {
-    ($logger:expr, $($arg:tt)*) => ($crate::_log($logger, $crate::LogLevel::Emergency, file!(), line!(), format_args!($($arg)*)));
+    ($logger:expr, $($arg:tt)*) => ($crate::_log(&$logger, $crate::LogLevel::Emergency, file!(), line!(), format_args!($($arg)*)));
 }
 
 #[macro_export]
 macro_rules! alert {
-    ($logger:expr, $($arg:tt)*) => ($crate::_log($logger, $crate::LogLevel::Alert, file!(), line!(), format_args!($($arg)*)));
+    ($logger:expr, $($arg:tt)*) => ($crate::_log(&$logger, $crate::LogLevel::Alert, file!(), line!(), format_args!($($arg)*)));
 }
 
 #[macro_export]
 macro_rules! critical {
-    ($logger:expr, $($arg:tt)*) => ($crate::_log($logger, $crate::LogLevel::Critical, file!(), line!(), format_args!($($arg)*)));
+    ($logger:expr, $($arg:tt)*) => ($crate::_log(&$logger, $crate::LogLevel::Critical, file!(), line!(), format_args!($($arg)*)));
 }
 
 #[macro_export]
 macro_rules! error {
-    ($logger:expr, $($arg:tt)*) => ($crate::_log($logger, $crate::LogLevel::Error, file!(), line!(), format_args!($($arg)*)));
+    ($logger:expr, $($arg:tt)*) => ($crate::_log(&$logger, $crate::LogLevel::Error, file!(), line!(), format_args!($($arg)*)));
 }
 
 #[macro_export]
 macro_rules! warning {
-    ($logger:expr, $($arg:tt)*) => ($crate::_log($logger, $crate::LogLevel::Warning, file!(), line!(), format_args!($($arg)*)));
+    ($logger:expr, $($arg:tt)*) => ($crate::_log(&$logger, $crate::LogLevel::Warning, file!(), line!(), format_args!($($arg)*)));
 }
 
 #[macro_export]
 macro_rules! notice {
-    ($logger:expr, $($arg:tt)*) => ($crate::_log($logger, $crate::LogLevel::Notice, file!(), line!(), format_args!($($arg)*)));
+    ($logger:expr, $($arg:tt)*) => ($crate::_log(&$logger, $crate::LogLevel::Notice, file!(), line!(), format_args!($($arg)*)));
 }
 
 #[macro_export]
 macro_rules! info {
-    ($logger:expr, $($arg:tt)*) => ($crate::_log($logger, $crate::LogLevel::Informational, file!(), line!(), format_args!($($arg)*)));
+    ($logger:expr, $($arg:tt)*) => ($crate::_log(&$logger, $crate::LogLevel::Informational, file!(), line!(), format_args!($($arg)*)));
 }
 
 #[macro_export]
 macro_rules! debug {
-    ($logger:expr, $($arg:tt)*) => ($crate::_log($logger, $crate::LogLevel::Debug, file!(), line!(), format_args!($($arg)*)));
+    ($logger:expr, $($arg:tt)*) => ($crate::_log(&$logger, $crate::LogLevel::Debug, file!(), line!(), format_args!($($arg)*)));
 }
